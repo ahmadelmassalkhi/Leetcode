f1de35e1e4d8f6a226721f9f3e88a8c2eab11ede
ahmadmassalkhi <massalkhiahmad18@gmail.com>
2024-08-25 21:17:43 +0300

    Add recursive solution for LinkedLists 21

--- Linked Lists/21. Merge Two Sorted Lists/solution.py
+++ Linked Lists/21. Merge Two Sorted Lists/solution.py
@@ -14,28 +14,20 @@ class Solution(object):
         """
         head = tail = None
         while list1 and list2:
-            if list1.val < list2.val:
-                if head == None:
-                    head = list1
-                else:
-                    tail.next = list1
-                tail = list1
+            minNode = list1 if list1.val < list2.val else list2
+            if head == None:
+                head = minNode
+            else:
+                tail.next = minNode
+            tail = minNode
+            if minNode == list1:
                 list1 = list1.next
             else:
-                if head == None:
-                    head = list2
-                else:
-                    tail.next = list2
-                tail = list2
                 list2 = list2.next
         if list1 == None and list2 == None:
             return head
-        if list1:
-            if tail == None:
-                return list1
-            tail.next = list1
-        if list2:
-            if tail == None:
-                return list2
-            tail.next = list2
+        nonNullList = list1 if list1 else list2
+        if tail == None:
+            return nonNullList
+        tail.next = nonNullList
         return head
